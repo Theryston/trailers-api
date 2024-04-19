a82241edcea6a07a9210adb4d4736742d58858dd
Theryston <devtheryston@gmail.com>
2024-04-19 11:15:24 -0300

    fix: use IGNORE_PROXY

--- src/clients/client-with-proxy.js
+++ src/clients/client-with-proxy.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 import axiosRetry from 'axios-retry';
 import axiosRetryConfig from './axios-retry-config.js';
 
-const clientWithProxy = axios.create({
-    proxy: {
-        host: process.env.PROXY_HOST,
-        port: process.env.PROXY_PORT,
-        auth: {
-            username: process.env.PROXY_USERNAME,
-            password: process.env.PROXY_PASSWORD
-        },
-        protocol: process.env.PROXY_PROTOCOL,
+const proxy = {
+    host: process.env.PROXY_HOST,
+    port: process.env.PROXY_PORT,
+    auth: {
+        username: process.env.PROXY_USERNAME,
+        password: process.env.PROXY_PASSWORD
     },
+    protocol: process.env.PROXY_PROTOCOL,
+}
+
+const clientWithProxy = axios.create({
+    proxy: process.env.IGNORE_PROXY === 'true' ? undefined : proxy,
 });
 
 axiosRetry(clientWithProxy, axiosRetryConfig);
