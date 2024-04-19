8d59411cf1bca68cb0de0870c568188af242f87d
Theryston <devtheryston@gmail.com>
2024-04-19 10:36:35 -0300

    fix: use arraybuffer

--- src/utils/download-file.js
+++ src/utils/download-file.js
@@ -12,19 +12,16 @@ axiosRetry(axios, {
 
 export default async function downloadFile({ url, path, append = false }) {
   const response = await axios.get(url, {
-    responseType: 'stream',
+    responseType: 'arraybuffer'
   });
 
-  const writer = fs.createWriteStream(path, { flags: append ? 'a' : 'w' });
+  const data = Buffer.from(response.data, 'binary');
 
-  return new Promise((resolve, reject) => {
-    response.
-      data
-      .pipe(writer)
-      .on('finish', () => {
-        resolve();
-      }).on('error', (err) => {
-        reject(err);
-      });
-  })
+  if (append) {
+    fs.appendFileSync(path, data);
+  } else {
+    fs.writeFileSync(path, data);
+  }
+
+  return path;
 }
