3389e7f29ab05c1667bd59a6a36a8ff0701b2b60
Theryston <devtheryston@gmail.com>
2024-09-03 10:26:41 -0300

    fix: trailers s3

--- src/temp-upload.js
+++ src/temp-upload.js
@@ -5,29 +5,32 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import mime from "mime-types";
 
 const client = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+  endpoint: process.env.S3_ENDPOINT,
+  region: process.env.S3_REGION,
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  },
 });
 
 export async function tempUpload(filePath) {
-    const fileName = path.basename(filePath);
-    const key = `${uuid()}-${fileName}`;
-    const mimeType = mime.lookup(fileName);
+  const fileName = path.basename(filePath);
+  const key = `${uuid()}-${fileName}`;
+  const mimeType = mime.lookup(fileName);
 
-    const command = new PutObjectCommand({
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: key,
-        Body: fs.createReadStream(filePath),
-        ACL: "public-read",
-        ContentType: mimeType || undefined
-    });
+  const command = new PutObjectCommand({
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key,
+    Body: fs.createReadStream(filePath),
+    ACL: "public-read",
+    ContentType: mimeType || undefined,
+  });
 
-    await client.send(command);
+  await client.send(command);
 
-    const url = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+  const urlEndpoint = new URL(process.env.S3_ENDPOINT);
 
-    return url;
+  const url = `https://${process.env.S3_BUCKET_NAME}.${urlEndpoint.host}/${key}`;
+
+  return url;
 }
